a99d6d570f2b02bc437f7a4a71abfc56eff4295e
amunlozb <amunlozb@g.educaand.es>
2024-01-12 13:43:11 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'home.html',
+      template: 'src/home.html',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'catalog.html',
+      template: 'src/catalog.html',
+    }),
+    new HtmlWebpackPlugin({
+        filename: 'login.html',
+        template: 'src/login.html',
+    }),
+    new CopyPlugin({
+        patterns: [
+          { from: "src/scss/img", to: "img" },
+        ],
+      }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer
+                ]
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
